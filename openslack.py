3c211cd7f03079d8358a29161e8e07ba80ef7a1f
frxnnk <frxnco@protonmail.com>
2023-06-08 22:32:18 -0300

    secure api keys

--- openslack.py
+++ openslack.py
@@ -1,29 +1,40 @@
 import os
+from dotenv import load_dotenv
 from slack_sdk import WebClient
 import openai
 
-slack_token_user = "xoxp-5417079717520-5390435807605-5379016664439-aa5c73e0aa5691fa2a980937c503bb4a"  # Set your Slack OAuth access token here
-slack_token_bot = "xoxb-5417079717520-5393356374355-T3AM05zT63f5pInOvfPEfXeW"
+# Cargar las variables del archivo .env
+load_dotenv()
 
-channel_id = "C05BK7F8UUT"  # Set the desired Slack channel ID here
+TOKEN_USER = os.environ.get('TOKEN_USER')
+TOKEN_BOT = os.environ.get('TOKEN_BOT')
+KEY_OPENAI = os.environ.get('KEY_OPENAI')
 
-foundUserClient = WebClient(token=slack_token_user)
-botUserClient = WebClient(token=slack_token_bot)
+print(TOKEN_USER,TOKEN_BOT)
 
-response = foundUserClient.users_identity()
+userClient = WebClient(TOKEN_USER)
+botClient = WebClient(TOKEN_BOT)
+
+# Get user ID
+response = userClient.users_identity()
 user_id = response["user"]["id"]
 
-channel_info = foundUserClient.conversations_info(channel=channel_id)
+# Set the desired Slack channel ID and get channel name
+channel_id = "C05BK7F8UUT"
+channel_info = botClient.conversations_info(channel=channel_id)
 channel_name = channel_info["channel"]["name"]
 
-response2 = botUserClient.conversations_history(channel=channel_id)
-messages = response2["messages"]
+# Get channel messages
+response = botClient.conversations_history(channel=channel_id)
+messages = response["messages"]
 
+# Create the content to summarize
 content = "\n".join([message["text"] for message in messages])
-print(channel_name)
-prompt = "Please provide a very short summary of the following channel of slack:\n\n" + content + "\n\n" + "I want the summary to have the title of the channel: " + channel_name
 
-openai.api_key = "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"  # Set your OpenAI API key here
+# Create the prompt to resume the summary
+prompt = f"Please provide a very short summary. Max 150 characters. The summary starts with the title: {channel_name}\n\nThe content to summarize is a channel slack conversation:\n\n{content}"
+
+openai.api_key = KEY_OPENAI # Set your OpenAI API key here
 
 response = openai.Completion.create(
     engine="text-davinci-003",
@@ -32,6 +43,8 @@ response = openai.Completion.create(
     temperature=0.5
 )
 
+# Resume the summary
 summary = response.choices[0].text.strip()
 
-botUserClient.chat_postMessage(channel=user_id, text=summary)
+# Send the summary to the private message channel of the user
+botClient.chat_postMessage(channel=user_id, text=summary)
